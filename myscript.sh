9fa192ac5db022200ee468ad7d9489c968ed004a
Eugene Dar <evgenedar@mail.ru>
2023-03-29 21:11:14 +0300

    testing some repo tests

--- myscript.sh
+++ myscript.sh
@@ -1,30 +1,55 @@
 # set system variable
 DYNAMORIO_HOME=/DynamoRIO-Linux-9.0.1
 
+
+
 # copying files to release
 cp /my2.cpp $DYNAMORIO_HOME/samples
 rm $DYNAMORIO_HOME/samples/CMakeLists.txt
 cp /CMakeLists.txt $DYNAMORIO_HOME/samples
+
+
 
 # create .so (from cloned repo we call release files)
 cd dynamorio && mkdir build && cd build
 cmake -DDynamoRIO_DIR=$DYNAMORIO_HOME/cmake $DYNAMORIO_HOME/samples
 make my2
 
+
+
 # build binary for testing
-cd /
-g++ -O0 -o app /main.cpp
+#cd /
+#g++ -O0 -o app /main.cpp
 
 # print binary disassembler
 #objdump -D /app > /disasm && cat /disasm
+
+
 
 # run my profiler
 #cd /DynamoRIO-Linux-9.0.1
 #bin64/drrun -c /dynamorio/build/bin/libmy2.so -only_from_app -- /app
 
+
+
 # run their profiler
+#cd /DynamoRIO-Linux-9.0.1
+#bin64/drrun -c samples/bin64/libinstrace_simple.so -only_from_app -- /app
+#cat /DynamoRIO-Linux-9.0.1/samples/bin64/instrace.app.* | grep "push" | wc -l
+
+
+
+# build tests from downloaded repo
+cd /fmt && mkdir build && cd build
+cmake ..
+make
+
+# run profiler for built tests
 cd /DynamoRIO-Linux-9.0.1
-bin64/drrun -c samples/bin64/libinstrace_simple.so -only_from_app -- /app
-cat /DynamoRIO-Linux-9.0.1/samples/bin64/instrace.app.* | grep "push" | wc -l
-
-
+FILES="/fmt/build/bin/*"
+for f in $FILES
+do
+  echo "Processing $f"
+  bin64/drrun -c samples/bin64/libinstrace_simple.so -only_from_app -- "$f"
+done
+cat /DynamoRIO-Linux-9.0.1/samples/bin64/instrace.* | grep "push" | wc -l
