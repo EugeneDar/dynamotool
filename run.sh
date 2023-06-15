ba7dc8fbab515920e513c4ee3ae744385f16841c
eugenedar <evgenedar@mail.ru>
2023-06-15 15:56:13 +0300

    parser implemented

--- run.sh
+++ run.sh
@@ -23,38 +23,19 @@ bash /collector.sh
 
 
 # run profiler for built tests
-echo "" > /logs/results_output.txt
+echo "" > /logs/log.txt
 cd /DynamoRIO-Linux-9.0.1
 FILES="/materials/*"
 for f in $FILES
 do
-  echo "Profiling file $f" >> /logs/results_output.txt
-  bin64/drrun -c /dynamorio/build/bin/libmy_profiler.so -only_from_app -- "$f" >> /logs/results_output.txt
+  echo "Profiling file $f" >> /logs/log.txt
+  bin64/drrun -c /dynamorio/build/bin/libmy_profiler.so -only_from_app -- "$f" >> /logs/log.txt
 
-  # this line profiles binary for heap allocations, so it's optional
-  valgrind "$f" >> /logs/results_output.txt 2>&1
+  # this line profiles binary for heap allocations, and it uses valgrind
+  valgrind "$f" >> /logs/log.txt 2>&1
 done
 
 
 
-# process logs to save only important info
-echo "" > /logs/processed.txt
-while IFS= read -r line; do
-    if [[ $line == *"Profiling file"* ]] || [[ $line == *"total heap usage"* ]] || [[ $line == *"Instrumentation results"* ]]; then
-        echo "$line" >> /logs/processed.txt
-    fi
-done < /logs/results_output.txt
-
-
-
-# process logs to save only numbers
-echo "" > /logs/numbers.txt
-while IFS= read -r line; do
-  if [[ $line == *"stack allocation instructions executed"* ]]; then
-    stack_allocations=$(grep -o '[0-9]\+' <<< "$line")
-    echo "Stack allocations: $stack_allocations" >> /logs/numbers.txt
-  elif [[ $line == *"total heap usage"* ]]; then
-    heap_allocations=$(echo "$line" | awk '{print $5}')
-    echo "Heap allocations: $heap_allocations" >> /logs/numbers.txt
-  fi
-done < /logs/processed.txt
+# parse logs
+python3 parser.py > /logs/data.csv
